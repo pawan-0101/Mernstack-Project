422e30de9faff6eb89a4a61f7a4aedd6eb037a79
pawan-0101 <pawancod01@gmail.com>
2025-07-10 15:17:25 +0530

    router and controller and middlewares, update

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//import routes
+
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users",userRouter)//prefix
+//http://localhost:8000/api/v1/users/register/login
+
 export {app}
